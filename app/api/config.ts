5c793742c1ced1af943b6a9f9317039e11849033
andres <andres857@gmaill.com>
2025-05-30 16:40:15 -0500

    add docker to project

--- app/api/config.ts
+++ app/api/config.ts
@@ -3,7 +3,7 @@
 export const API_CONFIG = {
     BASE_URL: process.env.NODE_ENV === 'production' 
       ? 'https://api.dominio.com'
-      : 'http://localhost:3020',
+      : 'http://localhost:5173',
     
     // Timeouts
     TIMEOUT: 10000, // 10 segundos
